f2dac84f3c00b050c931e55d56776cc4ddaea7fd
Jarek Zmudzinski <jarek@toronegro.net>
2012-04-11 15:36:26 +0200

    mass assignment protection

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  attr_accessible :file_path, :title, :cover
+  attr_accessible :file_path, :title, :cover, :user_id
 
   belongs_to :user
   has_and_belongs_to_many :playlists
